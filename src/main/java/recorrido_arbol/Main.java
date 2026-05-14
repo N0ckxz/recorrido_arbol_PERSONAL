914be71b369347da1b1bebae669d8c090ae9fcaa
Nicolás Alarcon <est.nicolas.alarcon@unimilitar.edu.co>
2026-05-14 08:59:10 -0500

    Implementar clase Node con estructura de arbol y metodos transversales

--- src/main/java/recorrido_arbol/Main.java
+++ src/main/java/recorrido_arbol/Main.java
@@ -2,6 +2,27 @@ package recorrido_arbol;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node root = new Node(1);
+        Node n1 = new Node(2);
+        Node n2 = new Node(3);
+        Node n3 = new Node(4);
+
+        Node n4 = new Node(5);
+        Node n5 = new Node(6);
+
+        Node n6 = new Node(7);
+    
+        root.addChild(n1);
+        root.addChild(n2);
+        root.addChild(n3);
+
+        n1.addChild(n4);
+        n1.addChild(n5);
+
+        n3.addChild(n6);
+
+        root.printTree();
+
+        root.search(6);
     }
 }
---
+++ src/main/java/recorrido_arbol/Node.java
@@ -0,0 +1,49 @@
+package recorrido_arbol;
+
+import java.util.LinkedList;
+import java.util.List; // Importamos List ya que lo tenemos mencionado en el atributo "List<Node>"
+
+public class Node {
+    int data = 0;
+    List<Node> children; // Para el arreglo dinámico
+
+    Node() { //Constructor por defecto 
+        this.children = new LinkedList<>();
+    }  
+    
+    Node(int x) { //Constructor principal
+        this.data = x;
+        this.children = new LinkedList<>(); //Inicializamos la lista
+    }
+
+    void addChild(Node node) {
+        this.children.add(node);
+    }
+
+    void printTree() {
+        System.out.print(this.data + " ");
+        
+        for(int i = 0; i < children.size(); i++) {
+            children.get(i).printTree(); // Recursion para que el metodo se ejecute por cada hijo del arbol
+        }
+    }
+
+    Node search(int searchValue) {
+
+        if(this.data == searchValue) { //Si el dato del nodo actual es el mismo que el dato ingresado, verdadero
+            return this;
+        } 
+
+        // Buscamos en los hijos
+        for (int i = 0; i <children.size(); i++) {
+            //Con ayuda de Gemini
+            Node findInChild = children.get(i).search(searchValue);
+            // Si se encuentra dentro de un hijo, mandamos el true hacia arriba
+            if (findInChild != null) { // ERROR CORREGIDO: En Java debemos comparar contra null
+                return findInChild; // ERROR CORREGIDO: Debes retornar 'findInChild', 'node' no existía aquí
+            }
+        }
+        // No se encontro en ningun hijo
+        return null;
+    }
+}
