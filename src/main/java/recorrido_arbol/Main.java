269c2a4e1bbf427829e0ef0363eee2eb18d879d8
Nicolás Alarcon <est.nicolas.alarcon@unimilitar.edu.co>
2026-05-14 09:18:07 -0500

    Parametrizamos el uso de enteros en las declaraciones

--- src/main/java/recorrido_arbol/Main.java
+++ src/main/java/recorrido_arbol/Main.java
@@ -2,15 +2,15 @@ package recorrido_arbol;
 
 public class Main {
     public static void main(String[] args) {
-        Node root = new Node(1);
-        Node n1 = new Node(2);
-        Node n2 = new Node(3);
-        Node n3 = new Node(4);
+        Node<Integer> root = new Node<>(1);
+        Node<Integer> n1 = new Node<>(2);
+        Node<Integer> n2 = new Node<>(3);
+        Node<Integer> n3 = new Node<>(4);
 
-        Node n4 = new Node(5);
-        Node n5 = new Node(6);
+        Node<Integer> n4 = new Node<>(5);
+        Node<Integer> n5 = new Node<>(6);
 
-        Node n6 = new Node(7);
+        Node<Integer> n6 = new Node<>(7);
     
         root.addChild(n1);
         root.addChild(n2);
