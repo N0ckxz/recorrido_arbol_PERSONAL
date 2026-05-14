08938172e05c64f487f21623d6f8384d3e6a3fdc
Nicolás Alarcon <est.nicolas.alarcon@unimilitar.edu.co>
2026-05-14 09:13:33 -0500

    Actualizamos la clase Node para utilizar datos genericos

--- src/main/java/recorrido_arbol/Node.java
+++ src/main/java/recorrido_arbol/Node.java
@@ -2,21 +2,21 @@ package recorrido_arbol;
 
 import java.util.LinkedList;
 import java.util.List; // Importamos List ya que lo tenemos mencionado en el atributo "List<Node>"
-
-public class Node {
-    int data = 0;
-    List<Node> children; // Para el arreglo dinámico
+    
+public class Node<T> {
+    private T data = null;
+    private List<Node<T>> children; // Para el arreglo dinámico
 
     Node() { //Constructor por defecto 
         this.children = new LinkedList<>();
     }  
     
-    Node(int x) { //Constructor principal
-        this.data = x;
+    Node(T data) { //Constructor principal
+        this.data = data;
         this.children = new LinkedList<>(); //Inicializamos la lista
     }
 
-    void addChild(Node node) {
+    void addChild(Node<T> node) {
         this.children.add(node);
     }
 
@@ -28,16 +28,16 @@ public class Node {
         }
     }
 
-    Node search(int searchValue) {
+    Node<T> search(T searchValue) {
 
-        if(this.data == searchValue) { //Si el dato del nodo actual es el mismo que el dato ingresado, verdadero
+        if(this.data.equals(searchValue)) { //Si el dato del nodo actual es el mismo que el dato ingresado, verdadero
             return this;
         } 
 
         // Buscamos en los hijos
         for (int i = 0; i <children.size(); i++) {
             //Con ayuda de Gemini
-            Node findInChild = children.get(i).search(searchValue);
+            Node<T> findInChild = children.get(i).search(searchValue);
             // Si se encuentra dentro de un hijo, mandamos el true hacia arriba
             if (findInChild != null) { // ERROR CORREGIDO: En Java debemos comparar contra null
                 return findInChild; // ERROR CORREGIDO: Debes retornar 'findInChild', 'node' no existía aquí
